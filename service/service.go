f5b4be935220c41193b82dc06ee822cbde81c2dc
burt yu <ybt7755221@gmail.com>
2020-04-20 18:25:13 +0800

    提交变化，proto文件使用m2p生成的文件处理

--- service/service.go
+++ service/service.go
@@ -1,13 +1,1 @@
 package service
-
-import (
-	users_proto "grpc-server/protos/users"
-)
-
-func resultString(code int32, msg string, data string) *users_proto.FindRes {
-	res := new(users_proto.FindRes)
-	res.Code = code
-	res.Msg = msg
-	res.Data = data
-	return res
-}
--- service/userService.go
+++ service/userService.go
@@ -6,7 +6,7 @@ import (
 	"grpc-server/entity"
 	"grpc-server/library/gutil"
 	"grpc-server/model"
-	users_proto "grpc-server/protos/users"
+	gin_users_proto "grpc-server/protos/gin_users"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -18,11 +18,11 @@ type UserServer struct {
 
 //注册服务
 func (u *UserServer) Register(s *grpc.Server) {
-	users_proto.RegisterGinUsersServer(s, u)
+	gin_users_proto.RegisterGinUsersServer(s, u)
 }
 
 //查询服务-带分页
-func (u *UserServer) FindUsers(ctx context.Context, in *users_proto.QuerySchema) (*users_proto.FindRes, error) {
+func (u *UserServer) FindByPagination(ctx context.Context, in *gin_users_proto.QuerySchema) (*gin_users_proto.FindRes, error) {
 	ginUsers := new(entity.GinUsers)
 	gutil.BeanUtil(ginUsers, in.Conditions)
 	ginUsersQuery := entity.GinUsersQuery{}
@@ -31,23 +31,33 @@ func (u *UserServer) FindUsers(ctx context.Context, in *users_proto.QuerySchema)
 	ginUsersQuery.PageSize = int(in.PageSize)
 	fmt.Println(ginUsersQuery)
 	ginUsersList, err := u.userModel.Find(ginUsersQuery)
+	result := new(gin_users_proto.FindRes)
 	if err != nil {
-		return resultString(entity.ERROR, err.Error(), ""), err
+		result.Code = entity.ERROR
+		result.Msg = err.Error()
+		result.Data = ""
+		return result, err
 	}
 	byteData, err := json.Marshal(ginUsersList)
 	if err != nil {
-		return resultString(entity.ERROR, err.Error(), ""), err
+		result.Code = entity.ERROR
+		result.Msg = err.Error()
+		result.Data = ""
+		return result, err
 	}
-	return resultString(entity.SUCCESS, entity.GetResultInfo(entity.SUCCESS), string(byteData)), nil
+	result.Code = entity.SUCCESS
+	result.Msg = entity.GetResultInfo(entity.SUCCESS)
+	result.Data = string(byteData)
+	return result, nil
 }
 
 //查询单条
-func (u *UserServer) FindOne(ctx context.Context, in *users_proto.GinUsersSchema) (*users_proto.FindOneRes, error) {
+func (u *UserServer) FindOne(ctx context.Context, in *gin_users_proto.GinUsersSchema) (*gin_users_proto.FindOneRes, error) {
 	ginUsers := new(entity.GinUsers)
 	gutil.BeanUtil(ginUsers, in)
 	ginUsersRes, err := u.userModel.Get(*ginUsers)
 	gutil.BeanUtil(in, &ginUsersRes)
-	res := new(users_proto.FindOneRes)
+	res := new(gin_users_proto.FindOneRes)
 	res.Code = 1000
 	res.Msg = "ok"
 	res.Data = in
@@ -55,12 +65,12 @@ func (u *UserServer) FindOne(ctx context.Context, in *users_proto.GinUsersSchema
 }
 
 //创建
-func (u *UserServer) Create(ctx context.Context, in *users_proto.GinUsersSchema) (res *users_proto.FindOneRes, err error) {
+func (u *UserServer) Create(ctx context.Context, in *gin_users_proto.GinUsersSchema) (res *gin_users_proto.FindOneRes, err error) {
 	fmt.Println("CREATE")
 	ginUsers := new(entity.GinUsers)
 	gutil.BeanUtil(ginUsers, in)
 	ginUsersRes, err := u.userModel.Insert(*ginUsers)
-	res = new(users_proto.FindOneRes)
+	res = new(gin_users_proto.FindOneRes)
 	if err != nil {
 		res.Code = entity.ERROR
 		res.Msg = err.Error()
@@ -74,13 +84,20 @@ func (u *UserServer) Create(ctx context.Context, in *users_proto.GinUsersSchema)
 }
 
 //更新
-func (u *UserServer) Update(ctx context.Context, in *users_proto.UpdateSchema) (*users_proto.FindRes, error) {
+func (u *UserServer) Update(ctx context.Context, in *gin_users_proto.UpdateSchema) (*gin_users_proto.FindRes, error) {
 	updateForm := new(entity.GinUsersUpdateForm)
 	gutil.BeanUtil(&updateForm.Conditions, in.Conditions)
 	gutil.BeanUtil(&updateForm.Modifies, in.Modifies)
 	aff, err := u.userModel.Update(updateForm.Conditions, updateForm.Modifies)
+	result := new(gin_users_proto.FindRes)
 	if err != nil {
-		return resultString(entity.ERROR, err.Error(), ""), err
+		result.Code = entity.ERROR
+		result.Msg = err.Error()
+		result.Data = ""
+		return result, err
 	}
-	return resultString(entity.SUCCESS, entity.GetResultInfo(entity.SUCCESS), fmt.Sprintf("affect lines: %d", aff)), err
+	result.Code = entity.SUCCESS
+	result.Msg = entity.GetResultInfo(entity.SUCCESS)
+	result.Data = fmt.Sprintf("affect lines: %d", aff)
+	return result, err
 }
